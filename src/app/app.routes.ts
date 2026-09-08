cc18740f68c8ce7e02a77c08ece3351d83c2dc21
Lorena <lgm15221@gmail.com>
2026-09-08 10:48:58 +0200

    Creados app/src/pages/about, app/src/pages/contac, app/src/pages/home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then(m => m.About)
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./pages/contact/contact').then(m => m.Contact)
+  }
+];
