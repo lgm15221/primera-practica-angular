155c9dd1758f3abdc1c53a344b0cf3fd396d0205
Lorena <lgm15221@gmail.com>
2026-09-08 12:13:29 +0200

    generated app/components/(card, title) and its configuration, plus 'Home.html' configuration

---
+++ src/app/components/title/title.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  imports: [],
+  templateUrl: './title.html',
+  styleUrl: './title.scss',
+})
+export class Title {
+  @Input() name = '';
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Card } from '../../components/card/card';
+import { Title } from '../../components/title/title';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, Title, Card],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
